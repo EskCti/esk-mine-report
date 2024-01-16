67c32103553fc24f110827253326f8be1ab203d4
eskokado <eskokado@gmail.com>
2024-01-16 07:31:35 -0300

    feat: create message KafkaEvent

---
+++ src/main/java/org/eskcti/mine/messages/KafkaEvent.java
@@ -0,0 +1,30 @@
+package org.eskcti.mine.messages;
+
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import jakarta.transaction.Transactional;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eskcti.mine.dto.ProposalDTO;
+import org.eskcti.mine.dto.QuotationDTO;
+import org.eskcti.mine.services.OpportunityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaEvent {
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvent.class);
+
+    OpportunityService opportunityService;
+
+    @Incoming("proposal-channel")
+    @Transactional
+    public void receiveProposal(ProposalDTO proposal) {
+        LOG.info("--- Recebendo Nova Proposta do Tópico Kafka ---");
+        opportunityService.buildOpportunity(proposal);
+    }
+
+    @Incoming("quotation-channel")
+    @Blocking
+    public void receiveQuotation(QuotationDTO quotation) {
+        LOG.info("--- Recebendo Nova Cotação de Moeda do Tópico Kafka ---");
+        opportunityService.saveQuotation(quotation);
+    }
+}
