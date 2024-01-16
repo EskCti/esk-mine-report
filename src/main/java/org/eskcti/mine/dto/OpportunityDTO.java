b943faa265fb29ec153eb7fb2113504c9c28f584
eskokado <eskokado@gmail.com>
2024-01-16 07:30:21 -0300

    feat: create dto Opportunity, Proposal and Quotation

---
+++ src/main/java/org/eskcti/mine/dto/OpportunityDTO.java
@@ -0,0 +1,22 @@
+package org.eskcti.mine.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
+
+@Builder
+@Data
+@AllArgsConstructor
+@Jacksonized
+public class OpportunityDTO {
+    private Long proposalId;
+
+    private String customer;
+
+    private BigDecimal priceTonne;
+
+    private BigDecimal lastDollarQuotation;
+}
