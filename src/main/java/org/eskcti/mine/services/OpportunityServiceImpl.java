37d74ddafb5cbcfd0fdf6daed0fd1a084c04a83b
eskokado <eskokado@gmail.com>
2024-01-17 06:31:03 -0300

    feat: create OpportunityService Impl

---
+++ src/main/java/org/eskcti/mine/services/OpportunityServiceImpl.java
@@ -0,0 +1,68 @@
+package org.eskcti.mine.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eskcti.mine.dto.OpportunityDTO;
+import org.eskcti.mine.dto.ProposalDTO;
+import org.eskcti.mine.dto.QuotationDTO;
+import org.eskcti.mine.entities.OpportunityEntity;
+import org.eskcti.mine.entities.QuotationEntity;
+import org.eskcti.mine.repositories.OpportunityRepository;
+import org.eskcti.mine.repositories.QuotationRepository;
+import org.eskcti.mine.utils.CSVHelper;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+@ApplicationScoped
+public class OpportunityServiceImpl implements OpportunityService{
+
+    @Inject
+    OpportunityRepository opportunityRepository;
+    @Inject
+    QuotationRepository quotationRepository;
+    @Override
+    public void buildOpportunity(ProposalDTO proposal) {
+        List<QuotationEntity> quotationEntities = quotationRepository.findAll().list();
+        Collections.reverse(quotationEntities);
+
+        OpportunityEntity opportunity = new OpportunityEntity();
+        opportunity.setDate(new Date());
+        opportunity.setProposalId(proposal.getProposalId());
+        opportunity.setCustomer(proposal.getCustomer());
+        opportunity.setPriceTonne(proposal.getPriceTonne());
+        opportunity.setLastDollarQuotation(quotationEntities.get(0).getCurrencyPrice());
+
+        opportunityRepository.persist(opportunity);
+    }
+
+    @Override
+    public void saveQuotation(QuotationDTO quotation) {
+
+    }
+
+    @Override
+    public List<OpportunityDTO> generateOpportunityDate() {
+        return null;
+    }
+
+    @Override
+    public ByteArrayInputStream generateCSVOpportunityReport() {
+        List<OpportunityDTO> opportunityList = new ArrayList<>();
+
+        opportunityRepository.findAll().list().forEach(item -> {
+            OpportunityDTO opportunity = OpportunityDTO
+                    .builder()
+                    .proposalId(item.getProposalId())
+                    .customer(item.getCustomer())
+                    .priceTonne(item.getPriceTonne())
+                    .lastDollarQuotation(item.getLastDollarQuotation())
+                    .build();
+            opportunityList.add(opportunity);
+        });
+        return CSVHelper.OpportunitiesToCSV(opportunityList);
+    }
+}
