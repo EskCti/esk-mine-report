b9a16f1b6d7504d01c0b361521402db9a8fb806e
eskokado <eskokado@gmail.com>
2024-01-18 05:09:49 -0300

    fix: annotations in KafkaEvent

--- src/main/java/org/eskcti/mine/messages/KafkaEvent.java
+++ src/main/java/org/eskcti/mine/messages/KafkaEvent.java
@@ -1,6 +1,8 @@
 package org.eskcti.mine.messages;
 
 import io.smallrye.reactive.messaging.annotations.Blocking;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eskcti.mine.dto.ProposalDTO;
@@ -9,9 +11,11 @@ import org.eskcti.mine.services.OpportunityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@ApplicationScoped
 public class KafkaEvent {
     private final Logger LOG = LoggerFactory.getLogger(KafkaEvent.class);
 
+    @Inject
     OpportunityService opportunityService;
 
     @Incoming("proposal-channel")
@@ -22,7 +26,7 @@ public class KafkaEvent {
     }
 
     @Incoming("quotation-channel")
-    @Blocking
+    @Transactional
     public void receiveQuotation(QuotationDTO quotation) {
         LOG.info("--- Recebendo Nova Cotação de Moeda do Tópico Kafka ---");
         opportunityService.saveQuotation(quotation);
