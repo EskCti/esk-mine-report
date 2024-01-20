0d1c0bafdf266eb99bcd80b17a81dabf6069f1d9
eskokado <eskokado@gmail.com>
2024-01-20 06:52:23 -0300

    feat: impl annotation @Authenticated in controllers

--- src/main/java/org/eskcti/mine/controllers/OpportunityController.java
+++ src/main/java/org/eskcti/mine/controllers/OpportunityController.java
@@ -1,5 +1,7 @@
 package org.eskcti.mine.controllers;
 
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -7,17 +9,26 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.ServerErrorException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eskcti.mine.dto.OpportunityDTO;
 import org.eskcti.mine.services.OpportunityService;
 
 import java.util.Date;
+import java.util.List;
 
 @Path("/api/opportunity")
+@Authenticated
 public class OpportunityController {
     @Inject
     OpportunityService opportunityService;
+
+    @Inject
+    JsonWebToken jsonWebToken;
+
     @GET
     @Path("/report")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    @RolesAllowed({"user", "manager"})
     public Response generateReport() {
         try {
             return Response
@@ -33,5 +44,12 @@ public class OpportunityController {
         } catch (ServerErrorException e) {
             return Response.serverError().build();
         }
+    }
+
+    @GET
+    @Path("/data")
+    @RolesAllowed({"user", "manager"})
+    public List<OpportunityDTO> generateData() {
+        return opportunityService.generateOpportunityData();
     }
 }
